05e5a0651acaa6a428e0718c904ed4fc68c11604
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2023-01-27 10:09:42 +0200

    checks

--- frontent/src/components/ForgotPassword/ForgotPassword.jsx
+++ frontent/src/components/ForgotPassword/ForgotPassword.jsx
@@ -72,6 +72,7 @@ const ForgotPassword = () => {
         const responseData = await response.json();
         // console.log(responseData);
         console.log("hhh", responseData);
+        alert(responseData.msg)
         setMsgModal('Sent! Check your mail.');/* if the response is not ok, alert the user */
         handleShow();
         handleClickHome();

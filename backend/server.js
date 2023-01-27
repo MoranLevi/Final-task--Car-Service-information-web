4ce96c44b2fb5d2d598b9e4bc2226adbbbd7f577
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2023-01-27 10:26:42 +0200

    try in server side nodemailer

--- backend/server.js
+++ backend/server.js
@@ -235,30 +235,33 @@ app.post('/forgotPassword', (req, res) => {
                 text: 'Hello,\nEnter the following link to reset password:\nhttps://car-service-information-web-server.vercel.app/#/resetPassword'
             };
 
-            await new Promise((resolve, reject) => {
+            return new Promise((resolve, reject) => {
                 // send the email
                 transporter.sendMail(mailOptions, function(err,info){
                     if(err){
                         console.log(err);
+                        res.status(500)
+                        res.send(err)
                         return;
                     }
-                    console.log("sent: "+info.response);
+                    else {
+                        // define the response message
+                        const forgotPasswordMsg = {
+                            method: 'GET',
+                            headers: {'Content-Type': 'application/json'},
+                            body: JSON.stringify(
+                            {
+                                title: 'forgotPasswordMsg',
+                                signUpResult: 'OK',
+                            })
+                        }
+
+                        res.type('application/json')
+                        res.send(forgotPasswordMsg) // send the response
+                    }
+                    // console.log("sent: "+info.response);
                 })
             })
-
-            // define the response message
-            const forgotPasswordMsg = {
-                method: 'GET',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(
-                {
-                    title: 'forgotPasswordMsg',
-                    signUpResult: 'OK',
-                })
-            }
-
-            res.type('application/json')
-            res.send(forgotPasswordMsg) // send the response
         })
 })
 

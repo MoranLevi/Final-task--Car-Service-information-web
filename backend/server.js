837cae2e7005b184afb6bacb021e5a68f1f9f912
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2023-01-27 11:13:49 +0200

    try cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port =  process.env.PORT || 8000 // Port to listen on
 
 app.use(express.json());
 app.use(cors()); 
-app.options('*', cors())
+app.options('/forgotPassword', cors())
 
 // Define object with db config data
 const db_config = {
